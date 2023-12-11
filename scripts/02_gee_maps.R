325e3338e9d5dbea5b4beddbc1b0f475f9885dcf
Hamed Sharif <sh.hamed998@gmail.com>
2023-12-10 23:05:15 -0800

    visualizing gee model results

---
+++ scripts/02_gee_maps.R
@@ -0,0 +1,47 @@
+# plotting hourly, daily, and monthly maps of AET derived with developed model
+# in Google Earth Engine.
+library(tidyverse)
+library(data.table)
+library(sf)
+library(raster)
+library(terrainr)
+library(ggnewscale)
+library(ggspatial)
+library(scales)
+theme_set(theme_bw())
+
+# reading synoptic station data
+df_synpt <- fread("data/synoptic_data_etr.csv")
+
+# reading gee results
+ertf_paths <- list.files("data/gee", full.names = TRUE, pattern = "\\.tif$")
+etrf_series <- raster::stack(ertf_paths)
+et_hr <- calc(etrf_series, function(x) x * df_synpt$etr_mm_per_hr)
+et_day <- calc(etrf_series, function(x) x * df_synpt$etr_mm_per_day)
+
+# constructing data frame for plotting
+etday_df <- as.data.frame(et_day, xy = TRUE)
+colnames(etday_df) <- c("x", "y",
+                        stamp_date("June 19, 2016")(mdy(df_synpt$date_image)))
+etday_df <- etday_df %>%
+  pivot_longer(cols = c(3:14), 
+               names_to = "date", values_to = "et_mm_day")
+
+# plotting
+plt <- ggplot() +
+  geom_raster(data = etday_df, aes(x = x, y = y, fill = et_mm_day)) +
+  scale_fill_viridis_c(option = "D", na.value = "grey50", guide = "colourbar") +
+  scale_x_continuous(
+    limits = c(min(etday_df$x), max(etday_df$x)),
+    expand = c(0, 0), 
+  ) +
+  scale_y_continuous(
+    limits = c(min(etday_df$y), max(etday_df$y)),
+    expand = c(0, 0)
+  ) +
+  labs(x = "", y = "", color = "") +
+  theme_void() +
+  facet_wrap(vars(date), nrow = 4, ncol = 3)
+
+ggsave("figures/gee_daily_aet.pdf", plt, width = 8, height = 12, units = "in")
+
